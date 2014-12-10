50e6ab7d677782e346782b215149853a3bc85ae2
BWV1079 <amirberetar@gmail.com>
2014-12-10 20:38:06 +0200

    add some function into carcass

--- Thesaurus/carcass.cpp
+++ Thesaurus/carcass.cpp
@@ -44,8 +44,57 @@ Carcass::Carcass()
 
 }
 
+bool Carcass::conf_write(){return true;}
+void Carcass::conf_read(){}
+void Carcass::confUser_write(){}
+void Carcass::confUser_read(){}
+
 void Carcass::message(QString str, bool _modal)
 {
     MessageWindow* ms = new MessageWindow(str, _modal);
     ms->show();
+}
+
+QString Carcass::enumWToQStr (WriteResult wr)
+{
+    switch (wr) {
+    case WriteResult::OK:
+        return "OK";
+    case WriteResult::Write:
+        return "Write";
+    case WriteResult::Open:
+        return "Open";
+    case WriteResult::Copy:
+        return "Copy";
+    case WriteResult::DelTmpWhileCopy:
+        return "DelTmpWhileCopy";
+    case WriteResult::DelSource:
+        return "DelSource";
+    case WriteResult::DelTmp:
+        return "DelTmp";
+    case WriteResult::RenameTmp:
+        return "RenameTmp";
+    default:
+        return "Error";
+    }
+}
+
+QString Carcass::enumRToQStr (ReadResult rr)
+{
+    switch (rr) {
+    case ReadResult::OK:
+        return "OK";
+    case ReadResult::DelTmp:
+        return "DelTmp";
+    case ReadResult::NotFound:
+        return "NotFound";
+    case ReadResult::Open:
+        return "Open";
+    case ReadResult::Read:
+        return "Read";
+    case ReadResult::RenameTmp:
+        return "RenameTmp";
+    default:
+        return "Error";
+    }
 }
