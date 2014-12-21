d0e4d247c45db5d00db9660fce106b52dabdee85
BWV1079 <amirberetar@gmail.com>
2014-12-21 12:26:56 +0200

    comment LangList

--- Thesaurus/carcass.h
+++ Thesaurus/carcass.h
@@ -5,35 +5,46 @@
 
 class Carcass;
 
+//Список языков
 class LangList{
 public:
-    struct Lang{
-        QString name_eng;
-        QString name_native;
-        QIcon ico;
-        int keyboard;
-        QList<QChar> transcript;
-    };
-    struct StrIco{
-        StrIco(const QString* _name, const QIcon* _icon);
-        const QString* name;
-        const QIcon* icon;
-    };
+    struct Lang{                    //
+        QString name_eng;           //
+        QString name_native;        //
+        QIcon ico;                  // Структура для списка языков
+        int keyboard;               //
+        QList<QChar> transcript;    //
+    };                              //
+    struct StrIco{                                          //
+        StrIco(const QString* _name, const QIcon* _icon);   //
+        const QString* name;                                // Структура для списка имен и иконок языков
+        const QIcon* icon;                                  //
+    };                                                      //
+public:
+    //***************************************************
+    //                   Интерфейс
+    //***************************************************
+    const Lang* getLang(QString);
+    QList<StrIco> getStrIcoList();
+    bool Initialize();
+    //***************************************************
+    //                Служебная часть
+    //***************************************************
+public:
+    LangList(Carcass*);
+    friend QDataStream& operator>> (QDataStream& out, Lang& l);
+    friend QDataStream& operator<< (QDataStream& in, const Lang& l);
 private:
     Carcass* carcass;
     QList<Lang> Lang_List;
     bool initialized;
-public:
-    LangList(Carcass*);
-    const Lang* getLang(QString);
-    QList<StrIco> getStrIcoList();
-    bool Initialize();
-    friend QDataStream& operator>> (QDataStream& out, Lang& l);
-    friend QDataStream& operator<< (QDataStream& in, const Lang& l);
 private:
     bool ReadFile();
     bool WriteFile();
+    //***************************************************
 };
+
+
 //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 //структура для хранения списка созданных пользователем направлений изучения языка (Learning Direction)
 class L_D_List{
