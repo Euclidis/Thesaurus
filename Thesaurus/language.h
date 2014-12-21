5b4c04e60835fc4747400a571ae79d147fbc666f
BWV1079 <amirberetar@gmail.com>
2014-12-21 12:37:52 +0200

    Comment Language

--- Thesaurus/language.h
+++ Thesaurus/language.h
@@ -7,32 +7,36 @@
 
 class Language
 {
-//*****************************************
-//               INTERFACE
-//*****************************************
+    //*****************************************
+    //               Интерфейс
+    //*****************************************
 public:
     QString name;                       //Имя языка
     QList<Word> words;                  //Список слов языка
     QList<QString> dictionaries;        //Спиок словарей
 public:
     bool Initialize();                  //Обязательно необходимо вызвать при создании экземпляра класса
+
     bool Contains(const QString str);
     int  IndexOf(const QString str);
-    bool WriteFile();
-    bool ReadFile();
+
     void AddWord(Word &_word);
     void RemoveWord(QString);
     bool AddDictionary(QString);
     void RemoveDictionary(QString);
-//-----------------------------------------
+    //*****************************************
+    //            Служебная часть
+    //*****************************************
 public:
     Language(Carcass *_carcass);
-//*****************************************
 
 private:
     Carcass* carcass;
     QString adress;
     bool initialized;
+    bool WriteFile();
+    bool ReadFile();
+    //*****************************************
 };
 
 
